d5efd258b28df5a9c708dd4b19122c449f6dc1e4
Petr Šťastný <petr@soptik.tech>
2022-03-19 12:04:20 +0100

    Added exit code

--- Main.hs
+++ Main.hs
@@ -1,12 +1,18 @@
 import Parse.Parser (getAST)
 import System.Environment
+import System.Exit
+import Data.Either
 
 main :: IO ()
-main = getArgs >>= parseArgs >>= putStrLn
+main = getArgs >>= parseArgs >>= putStrLnAndExit
 
-parseArgs :: [String] -> IO String
-parseArgs [] = return "No input filename given."
+parseArgs :: [String] -> IO (String, ExitCode)
+parseArgs [] = return ("No input filename given.", ExitFailure 64)
 parseArgs [filename] = do
     file <- readFile filename
-    return (show (getAST filename file))
+    let ast = getAST filename file
+    if isLeft ast then return (show ast, ExitFailure 1) else return (show ast, ExitSuccess)
 parseArgs (x:xs) = parseArgs [x]
+
+putStrLnAndExit :: (String, ExitCode) -> IO a
+putStrLnAndExit (str, ec) = putStrLn str >> exitWith ec
