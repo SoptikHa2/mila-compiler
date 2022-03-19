b113572174715987271326e868ca899574f74f93
Petr Šťastný <petr@soptik.tech>
2022-03-19 12:09:39 +0100

    Added automated testing

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+green() {
+    echo -e '\e[32m'
+}
+normal() {
+    echo -e '\e[39m'
+}
+red() {
+    echo -e '\e[91m'
+}
+
+make compile
+
+echo "Testing parsing..."
+for file in samples/*; do
+    if ./MilaCompiler "$file" >/dev/null; then
+        echo "[$(green)ok$(normal)] $file"
+    else
+        echo "[$(red)fail$(normal)] $file"
+    fi
+done
